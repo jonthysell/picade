e4221b6982c4bbb6313eb702502860996a002a7c
Jon Thysell <thysell@gmail.com>
2024-04-17 12:56:05 -0700

    Add delay-less button debouncing to joystick code

--- software/picade-controller-arduino-joystick/picade-controller-joystick.c
+++ software/picade-controller-arduino-joystick/picade-controller-joystick.c
@@ -14,11 +14,14 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
   false, false,          // No rudder or throttle
   false, false, false);  // No accelerator, brake, or steering
 
+#define DEBOUNCE_DELAY_MS 5 // How long to debounce a button in MS
+
 typedef struct
 {
-  int  port;       // which port is this input on? 0 = PORTB, 1 = PORTF, 2 = PORTD
-  char mask;       // bitmask for the input on the port
-  boolean pressed; // is this input current pressed?
+  int  port;            // which port is this input on? 0 = PORTB, 1 = PORTF, 2 = PORTD
+  char mask;            // bitmask for the input on the port
+  boolean pressed;      // is this input current pressed?
+  uint32_t lastPressed; // what time was this button last pressed?
 } input;
 
 // PICADE: Switch to PCB Mappings
@@ -32,7 +35,6 @@ typedef struct
 //-------------------------------------------------------------------
 //           |  Select                              Enter  |
 
-
 input inputs[] = {
   // 0 is PORTB
   // 1 is PORTF
@@ -40,34 +42,34 @@ input inputs[] = {
   // 3 is PORTC
 
   /* Joystick UDLR on Port B-0123 */
-  { 0, B00000001, 0 }, // UP
-  { 0, B00000010, 0 }, // DOWN
-  { 0, B00000100, 0 }, // LEFT
-  { 0, B00001000, 0 },  //RIGHT
+  { 0, B00000001, false, 0 }, // UP
+  { 0, B00000010, false, 0 }, // DOWN
+  { 0, B00000100, false, 0 }, // LEFT
+  { 0, B00001000, false, 0 },  //RIGHT
 
   /* Buttons 123456 on Port F-01-4567 */
-  { 1, B10000000, 0 }, // Button 1
-  { 1, B01000000, 0 }, // Button 2
-  { 1, B00100000, 0 }, // Button 3
-  { 1, B00010000, 0 }, // Button 4
-  { 1, B00000010, 0 }, // Button 5
-  { 1, B00000001, 0 }, // Button 6
+  { 1, B10000000, false, 0 }, // Button 1
+  { 1, B01000000, false, 0 }, // Button 2
+  { 1, B00100000, false, 0 }, // Button 3
+  { 1, B00010000, false, 0 }, // Button 4
+  { 1, B00000010, false, 0 }, // Button 5
+  { 1, B00000001, false, 0 }, // Button 6
   
   /* GPIO 12/Maxi Buttons 78 on Port D-67 */
-  { 2, B01000000, 0 }, // GPIO 1 / Button 7
-  { 2, B10000000, 0 }, // GPIO 2 / Button 8
+  { 2, B01000000, false, 0 }, // GPIO 1 / Button 7
+  { 2, B10000000, false, 0 }, // GPIO 2 / Button 8
 
   /* GPIO 3456 on Port B-456 C-6 */
-  { 0, B00010000, 0 }, // GPIO 3 / Button 9
-  { 0, B00100000, 0 }, // GPIO 4 / Button 10
-  { 0, B01000000, 0 }, // GPIO 5 / Button 11
-  { 3, B01000000, 0 }, // GPIO 6 / Button 12
+  { 0, B00010000, false, 0 }, // GPIO 3 / Button 9
+  { 0, B00100000, false, 0 }, // GPIO 4 / Button 10
+  { 0, B01000000, false, 0 }, // GPIO 5 / Button 11
+  { 3, B01000000, false, 0 }, // GPIO 6 / Button 12
 
   /* Front/Side Buttons on Port D-0123 */
-  { 2, B00000001, 0 }, // 1UP Start / Button 13 
-  { 2, B00000010, 0 }, // 1UP Select/Coin / Button 14
-  { 2, B00000100, 0 }, // Enter / Button 15
-  { 2, B00001000, 0 }  // Esc / Button 16
+  { 2, B00000001, false, 0 }, // 1UP Start / Button 13 
+  { 2, B00000010, false, 0 }, // 1UP Select/Coin / Button 14
+  { 2, B00000100, false, 0 }, // Enter / Button 15
+  { 2, B00001000, false, 0 }  // Esc / Button 16
 
 };
 
@@ -99,58 +101,60 @@ void setup() {
 
 void loop() {
   /*
-    Check the state of each input, if it has changed then output the corresponding keypress
+    Check the state of each input and update the state of the Joystick object
   */
-  boolean changed = false;  // has any input changed?
+  uint32_t now = millis();
   char pinStates[4] = {PINB, PINF, PIND, PINC}; // read the current port states and store in 0, 1, 2 indexed array to match our port ids above
 
   // loop through each input
-  for(int i = 0; i < sizeof(inputs) / sizeof(input); i++)
+  for (int i = 0; i < sizeof(inputs) / sizeof(input); i++)
   {
-    // test for current state of this input
-    boolean test = ~pinStates[inputs[i].port] & inputs[i].mask;
+    // Get the current state of this input
+    boolean pressed = ~pinStates[inputs[i].port] & inputs[i].mask;
 
-    if(test != inputs[i].pressed) // has this input changed state since the last time we checked?
+    // Update our reported state if
+    if ( (!inputs[i].pressed && pressed) // Input went from unpressed to pressed (don't care about debounce) OR
+      || (inputs[i].pressed && !pressed && (now - inputs[i].lastPressed > DEBOUNCE_DELAY_MS)) // Input went from pressed to unpressed with debounce
+      ) 
     {
-      changed = true; // something changed, we should remember that
-      inputs[i].pressed = test; // update our state map so we know what's happening with this key in future
+      inputs[i].pressed = pressed;
+      inputs[i].lastPressed = now;
 
       switch (i) {
         case 0: // UP
-          if (test) {
+          if (pressed) {
             Joystick.setYAxis(-1);
           } else {
             Joystick.setYAxis(0);
           }
           break;
         case 1: // DOWN
-            if (test) {
+            if (pressed) {
               Joystick.setYAxis(1);
             } else {
               Joystick.setYAxis(0);
             }
             break;
         case 2: // LEFT
-            if (test) {
+            if (pressed) {
               Joystick.setXAxis(-1);
             } else {
               Joystick.setXAxis(0);
             }
             break;
         case 3: // RIGHT
-          if (test) {
+          if (pressed) {
             Joystick.setXAxis(1);
           } else {
             Joystick.setXAxis(0);
           }
           break;
         default: // Buttons 1 - 16
-          Joystick.setButton(i - 4, test);
+          Joystick.setButton(i - 4, pressed);
           break;
       }
     }
   }
 
-  if(changed) // if an input changed we'll add a small delay to avoid "bouncing" of the input
-    delay(10);
+  delay(1);
 }
